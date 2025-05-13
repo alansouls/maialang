2548a3f0c49f20061256c2b555df6c9487456eef
alansouls <maiaalan1111@gmail.com>
2025-05-13 22:13:04 +0100

    wip: interpreter using tokens

--- my_gc_language/interpreter.h
+++ my_gc_language/interpreter.h
@@ -9,15 +9,28 @@ namespace MaiaLang
 	class Expression;
 	struct MemoryAllocation;
 	class Scope;
+
 	class Interpreter
 	{
 	public:
 		void execute(const std::string& fileName, const std::string& codeData);
 	private:
-		static auto parseVariableDeclarationExpression(const std::smatch& variableDeclarationMatch) -> std::unique_ptr<Expression>;
-		static auto parseConstValueExpression(const std::smatch& constValueExpression) -> std::unique_ptr<Expression>;
-		static auto processExpression(Scope& scope, const std::string& expressionString) -> std::optional<MemoryAllocation>;
-		static auto parseExpression(const std::string& expressionString) -> std::unique_ptr<Expression>;
-		static auto parseFunctionCallExpression(const std::smatch& functionCallMatch) -> std::unique_ptr<Expression>;
+
+		struct StateMachine
+		{
+			enum State
+			{
+				VariableDeclaration,
+				FunctionDeclaration,
+				FunctionCall,
+				VariableValue,
+				VariableAssignment,
+				Unknown
+			};
+			
+			State state;
+			std::optional<std::string> typeName;
+			std::optional<std::string> symbolName;
+		};
 	};
 }
