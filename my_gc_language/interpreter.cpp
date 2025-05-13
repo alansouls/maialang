55d15bd2e125b1e0a3dda5a5791958264eb4845b
alansouls <maiaalan1111@gmail.com>
2025-05-13 22:58:36 +0100

    some more progress on interpreter and .clang-format file

--- my_gc_language/interpreter.cpp
+++ my_gc_language/interpreter.cpp
@@ -1,72 +1,93 @@
 #include "interpreter.h"
 #include "expression.h"
-#include "scope.h"
-#include "string_utils.h"
-#include <format>
-#include <regex>
 #include "lexer.h"
+#include "scope.h"
 
 namespace MaiaLang
 {
-	void Interpreter::execute(const std::string& fileName, const std::string& codeData) {
-		Scope fileScope(fileName, Scope::global().lock());
+void Interpreter::execute(const std::string &fileName, const std::string &codeData)
+{
+    Scope fileScope(fileName, Scope::global().lock());
 
-		Lexer lexer(codeData);
+    Lexer lexer(codeData);
 
-		std::vector<StateMachine> stateMachines;
+    std::vector<StateMachine> stateMachines;
 
-		for (auto& token : lexer.tokens()) {
-			if (stateMachines.empty()) {
-				if (token.type == Lexer::TokenType::Let) {
-					stateMachines.push_back({
-						.state = StateMachine::State::VariableDeclaration,
-						.typeName = std::optional<std::string>(),
-						.symbolName = std::optional<std::string>()
-					});
-				}
-				else if (token.type == Lexer::TokenType::Identitifer) {
-					stateMachines.push_back({
-						.state = StateMachine::State::Unknown,
-						.typeName = std::optional<std::string>(),
-						.symbolName = std::optional<std::string>()
-						});
-				}
-				else if (token.type == Lexer::TokenType::SemiCollon) {
-					continue;
-				}
-				else {
-					throw std::runtime_error("Invalid syntax");
-				}
-			}
+    for (auto &token : lexer.tokens())
+    {
+        if (stateMachines.empty())
+        {
+            if (token.type == Lexer::TokenType::Let)
+            {
+                stateMachines.push_back({.state = StateMachine::State::VariableDeclaration,
+                                         .typeName = std::optional<std::string>(),
+                                         .symbolName = std::optional<std::string>()});
+            }
+            else if (token.type == Lexer::TokenType::Identitifer)
+            {
+                stateMachines.push_back({.state = StateMachine::State::Unknown,
+                                         .typeName = std::optional<std::string>(),
+                                         .symbolName = std::optional<std::string>()});
+            }
+            else if (token.type == Lexer::TokenType::SemiCollon)
+            {
+                continue;
+            }
+            else
+            {
+                throw std::runtime_error("Invalid syntax");
+            }
+        }
 
-			auto& firstState = stateMachines[0];
+        auto &firstState = stateMachines[0];
 
-			if (firstState.state == StateMachine::State::VariableDeclaration) {
-				if (!firstState.typeName.has_value()) {
-					if (token.type == Lexer::TokenType::Identitifer) {
-						firstState.typeName = token.token;
-					}
-					else {
-						throw std::runtime_error("Expected type identifier");
-					}
-				}
-				else if (!firstState.symbolName.has_value()) {
-					if (token.type == Lexer::TokenType::Identitifer) {
-						firstState.typeName = token.token;
-					}
-					else {
-						throw std::runtime_error("Expected variable name");
-					}
-				}
-				else if (token.type == Lexer::TokenType::Assignment) {
+        if (firstState.state == StateMachine::State::VariableDeclaration)
+        {
+            handleVariableDeclaration(firstState, stateMachines, token);
+        }
+    }
 
-				}
-				else if (token.type != Lexer::TokenType::SemiCollon) {
-					throw std::runtime_error("")
-				}
-			}
-		}
+    GC::printInfo();
+}
 
-		GC::printInfo();
-	}
+void Interpreter::handleVariableDeclaration(StateMachine &firstState, std::vector<StateMachine> &stateMachines,
+                                            const Lexer::Token &token)
+{
+    if (!firstState.typeName.has_value())
+    {
+        if (token.type == Lexer::TokenType::Identitifer)
+        {
+            firstState.typeName = token.token;
+            return;
+        }
+
+        throw std::runtime_error("Expected type identifier");
+    }
+    else if (!firstState.symbolName.has_value())
+    {
+        if (token.type == Lexer::TokenType::Identitifer)
+        {
+            firstState.typeName = token.token;
+            return;
+        }
+
+        throw std::runtime_error("Expected variable name");
+    }
+
+    if (stateMachines.size() > 1)
+    {
+        auto &secondState = stateMachines[1];
+        return;
+    }
+    else if (token.type == Lexer::TokenType::Assignment)
+    {
+        stateMachines.push_back({.state = StateMachine::State::VariableAssignment});
+        return;
+    }
+
+    if (token.type != Lexer::TokenType::SemiCollon)
+    {
+        throw std::runtime_error("Invaid token, expected semicollon");
+    }
+}
 } // namespace MaiaLang
--- my_gc_language/interpreter.h
+++ my_gc_language/interpreter.h
@@ -1,36 +1,40 @@
 #pragma once
-#include <string>
-#include <span>
-#include <regex>
+#include "lexer.h"
 #include <optional>
+#include <regex>
+#include <span>
+#include <string>
 
 namespace MaiaLang
 {
-	class Expression;
-	struct MemoryAllocation;
-	class Scope;
+class Expression;
+struct MemoryAllocation;
+class Scope;
 
-	class Interpreter
-	{
-	public:
-		void execute(const std::string& fileName, const std::string& codeData);
-	private:
+class Interpreter
+{
+  public:
+    void execute(const std::string &fileName, const std::string &codeData);
 
-		struct StateMachine
-		{
-			enum State
-			{
-				VariableDeclaration,
-				FunctionDeclaration,
-				FunctionCall,
-				VariableValue,
-				VariableAssignment,
-				Unknown
-			};
-			
-			State state;
-			std::optional<std::string> typeName;
-			std::optional<std::string> symbolName;
-		};
-	};
-}
+  private:
+    struct StateMachine
+    {
+        enum State
+        {
+            VariableDeclaration,
+            FunctionDeclaration,
+            FunctionCall,
+            VariableValue,
+            VariableAssignment,
+            Unknown
+        };
+
+        State state;
+        std::optional<std::string> typeName;
+        std::optional<std::string> symbolName;
+    };
+
+    void handleVariableDeclaration(StateMachine &firstState, std::vector<StateMachine> &stateMachines,
+                                   const Lexer::Token &token);
+};
+} // namespace MaiaLang
